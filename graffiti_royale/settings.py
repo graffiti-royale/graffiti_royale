27ef1d3c2ffe384800cee21e533a319771954294
Rob Taylor <rob.taylor543@gmail.com>
2019-04-15 12:24:58 -0400

    Commented out STATIC_DIRS setting

--- graffiti_royale/settings.py
+++ graffiti_royale/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 ACCOUNT_ACTIVATION_DAYS = 7
