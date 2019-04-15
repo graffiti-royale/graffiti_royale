9c7db044ea1ebfebe0e01560a0567a240ef4aaed
Rob Taylor <rob.taylor543@gmail.com>
2019-04-15 12:51:47 -0400

    Changed path to STATIC_URL

--- graffiti_royale/settings.py
+++ graffiti_royale/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'core/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'core/static/')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
