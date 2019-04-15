d15c5bd4eda793f19be542144fd0b65d91de6437
Rob Taylor <rob.taylor543@gmail.com>
2019-04-15 13:08:47 -0400

    Reverted to working commit

--- graffiti_royale/urls.py
+++ graffiti_royale/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +28,3 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += staticfiles_urlpatterns()
