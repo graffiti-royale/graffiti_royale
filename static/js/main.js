2ab614cb7f3c35855b21181faae6a29caa4e83a2
Christian <ChristianLMedlin@gmail.com>
2019-04-17 15:54:52 -0400

    JavaScript now only runs on the appropriate pages

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,18 @@
-const canvas = document.querySelector('canvas')
-context = canvas.getContext('2d')
+function playPageJavaScript(){
+  const canvas = document.querySelector('canvas')
+  context = canvas.getContext('2d')
 
-let paint
-let myPath = []
-let room = document.URL.split('/')[3]
-let drawSocket = new WebSocket(`wss://${window.location.host}/ws/draw/${room}/`)
+  let paint
+  let myPath = []
+  let room = document.URL.split('/')[3]
+  let drawSocket = new WebSocket(`ws://${window.location.host}/ws/draw/${room}/`)
 
-let colorsArray = ['#070404', '#df4b26', '#040507', '#32ED2C', '#13d9f3', '#f313f3', '#f3ef13']
-color = colorsArray[Math.floor(Math.random() * colorsArray.length)]
+  let colorsArray = ['#070404', '#df4b26', '#040507', '#32ED2C', '#13d9f3', '#f313f3', '#f3ef13']
+  color = colorsArray[Math.floor(Math.random() * colorsArray.length)]
 
-let username = document.querySelector('.username').dataset.username
+  let username = document.querySelector('.username').dataset.username
 
-drawSocket.onmessage = function (e) {
+  drawSocket.onmessage = function (e) {
   let data = JSON.parse(e.data)
   context.strokeStyle = data['color']
   context.shadowBlur = 4
@@ -22,26 +23,26 @@ drawSocket.onmessage = function (e) {
   context.moveTo(data['path'][0][0], data['path'][0][1])
   context.lineTo(data['path'][1][0], data['path'][1][1])
   context.stroke()
-}
+  }
 
-canvas.addEventListener('mousedown', function(event) {
+  canvas.addEventListener('mousedown', function(event) {
   var mouseX = event.pageX - this.offsetLeft;
   var mouseY = event.pageY - this.offsetTop;
   paint = true
   myPath.push([mouseX, mouseY])
-})
+  })
 
-canvas.addEventListener('mouseup', function(event) {
+  canvas.addEventListener('mouseup', function(event) {
   paint = false
   myPath = []
-})
+  })
 
-canvas.addEventListener('mouseleave', function(event) {
+  canvas.addEventListener('mouseleave', function(event) {
   paint = false
   myPath = []
-})
+  })
 
-canvas.addEventListener('mousemove', function(event) {
+  canvas.addEventListener('mousemove', function(event) {
   if (paint) {
     myPath.push([event.pageX - this.offsetLeft, event.pageY - this.offsetTop])
     drawSocket.send(JSON.stringify({
@@ -59,7 +60,14 @@ canvas.addEventListener('mousemove', function(event) {
     context.stroke()
     myPath.shift()
   }
-})
+  })
 
-module.exports = {
+  module.exports = {
+  }
+}
+
+let onPlayPage = document.querySelector("#playPage");
+
+if (onPlayPage){
+  playPageJavaScript();
 }
