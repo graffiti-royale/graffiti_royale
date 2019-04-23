8823c78153fdd4c733bb1dd192033a95fb60cd12
Rob Taylor <rob.taylor543@gmail.com>
2019-04-23 11:42:04 -0400

    Merging master

--- core/models.py
+++ core/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .views import get_random_word
-import random
 
 class Room(models.Model):
     users = models.ManyToManyField(User, related_name='room', blank=True)
