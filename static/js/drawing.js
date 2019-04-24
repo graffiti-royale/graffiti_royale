5ab1b5d77eeadc93b34c9df7e3c9134f7507437c
MichaelTaylor96 <michael96taylor@gmail.com>
2019-04-23 21:50:37 -0400

    Added wordList changes

--- static/js/drawing.js
+++ static/js/drawing.js
@@ -41,6 +41,7 @@ function drawingScript2 () {
   let random_word = document.querySelector('.user_data').dataset.word
   let username = document.querySelector('.user_data').dataset.username
   let room = document.querySelector('.user_data').dataset.room
+  let wordList = []
 
   let userPaths = {}
   let usersSocket = new WebSocket(`wss://${window.location.host}/ws/${room}/users/`)
@@ -68,6 +69,11 @@ function drawingScript2 () {
     console.log(`Users updated:`)
     console.log(userPaths)
     console.log(data['room'])
+    wordList = []
+    for (let user of Object.values(userPaths)) {
+        wordList.push(user['word'])
+    }  
+    console.log(wordList)
   }
 
   window.addEventListener('beforeunload', function () {
@@ -263,6 +269,19 @@ function drawingScript2 () {
 
   module.exports = {}
 }
+
+// This is currently not bundling into our bundle.js, so none of it works on the site.
+// We will likely need an array of all currently active words, then I can correctly see if one of the words was submitted. I will need to talk to Michael to figure out how to index up our score when correctly submitting a word.
+
+// This currently only works when you hit the submit button, we will need to find a way to submit when you press the enter key. I tried searching for the answer online, but everything I found needed the use of jQuery.
+
+// let wordGuessed = document.querySelector(".wordGuessed");
+// let submitWordGuessed = document.querySelector(".submitWordGuessed");
+
+// submitWordGuessed.onclick = function(){console.log(wordGuessed.value)
+//     if(wordGuessed.value == "This will be our wordlist from Django"){
+//     }
+// }
 
 let onPlayPage = document.querySelector('#playPage')
 
