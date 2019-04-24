9390df87f4abf0e13b8a86e70303d40cf2e241b6
Rob Taylor <rob.taylor543@gmail.com>
2019-04-23 21:29:06 -0400

    Revert "Merge branch 'master' of https://github.com/rob-taylor543/graffiti_royale"
    This reverts commit 8365f75c78b0adc5d7495c5b95c92b39b6b9b2d2, reversing
    changes made to 51992b1f13f13e3faf9d2add39f1952467f43c69.

--- static/js/drawing.js
+++ static/js/drawing.js
@@ -1,16 +1,17 @@
+// window.onload = function(){
 function drawingScript2 () {
   /* Setting up the canvas */
   const drawMap = document.querySelector('#drawMap')
-  const miniMap = document.querySelector('#miniMap')
+  const miniMap = document.querySelector('#upcanvas')
   const drawMapCxt = drawMap.getContext('2d')
   miniMapCxt = miniMap.getContext('2d')
   drawMap.width = window.innerWidth
   drawMap.height = window.innerHeight
   miniMap.width = 600
   miniMap.height = 600
-//   drawMapCxt.shadowBlur = 3
-//   drawMapCxt.lineCap = 'round'
-//   drawMapCxt.lineWidth = 4
+  drawMapCxt.shadowBlur = 4
+  drawMapCxt.lineCap = 'round'
+  drawMapCxt.lineWidth = 4
 
   let paint
   let zoomedOut = true
@@ -19,14 +20,6 @@ function drawingScript2 () {
   let zoomCenter = []
   let xOffset = 0
   let yOffset = 0
-  let wordList
-  let cleanedWordList
-
-  /* Setting up visuals */
-  const bricks = document.querySelector('#bricks')
-  console.log(bricks)
-  const background = document.querySelector('#background')
-  console.log(background)
 
   /* Setting up personal info */
   let colorsArray = ['#070404', '#df4b26', '#040507', '#32ED2C', '#13d9f3', '#f313f3', '#f3ef13']
@@ -36,6 +29,8 @@ function drawingScript2 () {
   let room = document.querySelector('.user_data').dataset.room
 
   let userPaths = {}
+  let wordList = []
+  let cleanedWordList = []
   let usersSocket = new WebSocket(`wss://${window.location.host}/ws/${room}/users/`)
   usersSocket.onopen = function (event) {
     console.log(username)
@@ -50,78 +45,19 @@ function drawingScript2 () {
   usersSocket.onmessage = function (event) {
     let data = JSON.parse(event.data)
     userPaths = data['users']
+
     console.log(`Users updated:`)
     console.log(userPaths)
     console.log(data['room'])
-  }
 
-  window.addEventListener('beforeunload', function () {
-    console.log('closing!')
-    usersSocket.send(JSON.stringify({
-      'username': username,
-      'enter': false,
-      'color': myColor,
-      'random_word': random_word
-    }))
-    usersSocket.close()
-  })
+    // This should append to the wordList array whenever a new user joins the game.
 
-  miniMap.addEventListener('mousemove', function (event) {
-    if (zoomedOut) {
-      let X = Math.min(
-        Math.max(event.pageX - this.offsetLeft, drawMap.width / ZOOMFACTOR / 2), miniMap.width - drawMap.width / ZOOMFACTOR / 2
-      ) * ZOOMFACTOR
-      let Y = Math.min(
-        Math.max(event.pageY - this.offsetTop, drawMap.height / ZOOMFACTOR / 2),
-        miniMap.height - drawMap.height / ZOOMFACTOR / 2
-      ) * ZOOMFACTOR
-      zoomCenter = [Math.floor(X), Math.floor(Y)]
+    for (let user of Object.values(userPaths)) {
+      wordList.push(user['word'])
     }
-  })
-
-  miniMap.addEventListener('dblclick', function (event) {
-    zoomedOut = false
-    xOffset = zoomCenter[0] - drawMap.width / 2
-    yOffset = zoomCenter[1] - drawMap.height / 2
-    drawMap.style.zIndex = 4
-    miniMap.style.zIndex = 1
-    let moveX = -1 * (((zoomCenter[0] / ZOOMFACTOR) - (window.innerWidth / 2)) + miniMap.offsetLeft)
-    let moveY = -1 * (((zoomCenter[1] / ZOOMFACTOR) - (window.innerHeight / 2)) + miniMap.offsetTop)
-    X = zoomCenter[0] * 100 / miniMap.width / ZOOMFACTOR
-    Y = zoomCenter[1] * 100 / miniMap.height / ZOOMFACTOR
-    let coord = `${X}% ${Y}%`
-    console.log(coord)
-    bricks.style.transform = `translate(${moveX}px, ${moveY}px) scale(${ZOOMFACTOR}, ${ZOOMFACTOR})`
-    bricks.style.transformOrigin = coord
-    console.log(bricks.style)
-  })
-
-  drawMap.addEventListener('dblclick', function (event) {
-    zoomedOut = true
-    drawMap.style.zIndex = 1
-    miniMap.style.zIndex = 4
-    bricks.style.transform = "scale(1, 1)"
-    drawMapCxt.clearRect(0, 0, drawMap.width, drawMap.height)
-  })
-
-  let drawSocket = new WebSocket(`wss://${window.location.host}/ws/${room}/draw/`)
-
-  drawSocket.onmessage = function (event) {
-    let data = JSON.parse(event.data)
-    if (data['username'] != username) {
-      if (data['new_path']) {
-        userPaths[data['username']]['paths'].push(data['point'])
-      } else {
-        userPaths[data['username']]['paths'][userPaths[data['username']]['paths'].length - 1].push(data['point'])
-
-      //This should append to the wordList array whenever a new user joins the game.
-      for (let user of Object.values(userPaths)) {
-          wordList.push(user['word'])   
-        }
-      for (let i of wordList){
-        if(cleanedWordList.includes(i) == false){
-          cleanedWordList.push(i)
-        }
+    for (let i of wordList) {
+      if (cleanedWordList.includes(i) === false) {
+        cleanedWordList.push(i)
       }
     }
     console.log(cleanedWordList)
@@ -258,12 +194,11 @@ function drawingScript2 () {
       for (let user of Object.values(userPaths)) {
         let color = user['color']
         let paths = user['paths']
-
         drawMapCxt.strokeStyle = color
         drawMapCxt.shadowColor = color
-        drawMapCxt.shadowBlur = 4
+        drawMapCxt.shadowBlur = 3
         drawMapCxt.lineCap = 'round'
-        drawMapCxt.lineWidth = 6
+        drawMapCxt.lineWidth = 5
 
         for (let path of Object.values(paths)) {
           drawMapCxt.beginPath()
@@ -300,11 +235,23 @@ function drawingScript2 () {
   module.exports = {}
 }
 
+// This is currently not bundling into our bundle.js, so none of it works on the site.
+// We will likely need an array of all currently active words, then I can correctly see if one of the words was submitted. I will need to talk to Michael to figure out how to index up our score when correctly submitting a word.
+
+// This currently only works when you hit the submit button, we will need to find a way to submit when you press the enter key. I tried searching for the answer online, but everything I found needed the use of jQuery.
+
+// let wordGuessed = document.querySelector(".wordGuessed");
+// let submitWordGuessed = document.querySelector(".submitWordGuessed");
+
+// submitWordGuessed.onclick = function(){console.log(wordGuessed.value)
+//     if(wordGuessed.value == "This will be our wordlist from Django"){
+//     }
+// }
+
 let onPlayPage = document.querySelector('#playPage')
 
 if (onPlayPage) {
-  document.addEventListener('DOMContentLoaded', function() {
-      drawingScript2()
-  })
+  drawingScript2()
 }
-  
+
+// }
