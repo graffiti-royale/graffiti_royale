94381121b30448f35657f89689cd9add26680263
Rob Taylor <rob.taylor543@gmail.com>
2019-04-26 14:57:42 -0400

    added ping and pong methods to the start socket

--- static/js/waiting_room.js
+++ static/js/waiting_room.js
@@ -5,16 +5,36 @@ function waitingRoomJS () {
   let username = document.querySelector('.user_data').dataset.username
   console.log(username)
   console.log(full)
+
   let startSocket = new WebSocket(`wss://${window.location.host}/ws/${room}/start/`)
+
   startSocket.onopen = function (event) {
+    let messageType
     if (full === 'True') {
-      startSocket.send(JSON.stringify({
-        'startgame': true
-      }))
+      messageType = 'startgame'
+    } else {
+      messageType = 'ping'
     }
+    startSocket.send(JSON.stringify({
+      'messageType': messageType,
+      'username': username
+    }))
   }
-  startSocket.onmessage = function () {
-    window.location.href = `https://${window.location.host}/play/${room}/${username}/`
+
+  startSocket.onmessage = function (event) {
+    let data = JSON.parse(event.data)
+
+    if (data['type'] === 'start') {
+      window.location.href = `https://${window.location.host}/play/${room}/${username}/`
+    } else if (data['type'] === 'ping') {
+      startSocket.send(JSON.stringify({
+        'messageType': 'pong',
+        'ponger': username,
+        'pinger': data['pinger']
+      }))
+    } else if (data['type'] === 'pong') {
+      // add the pong name to the list of names for the specific pinger
+    }
   }
 }
 
