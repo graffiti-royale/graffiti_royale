824a8edaeae6efdb006c09c86520e7c436321719
Ryan Catrine <ryancatrine@gmail.com>
2019-04-26 19:51:20 -0400

    Edited tutorial and waiting room.

--- static/js/hints.js
+++ static/js/hints.js
@@ -1,12 +1,12 @@
 
-<div class='hints'>
-<div class='hints-title'>HiNTS</div>
+{ /* <div class='hints'>
+<div class='hints-title'>HiNTS</div> */ }
 
-Draw the word displayed at the top left
-Doubletap to zoom in/out
-You must be zoomed in to draw
-+1 point for guessing a drawing correctly
-+1 point if your drawing is guessed
-Draw or guess at any time
-View scoreboard by tapping&nbsp<i class='fas fa-caret-down scoreboard-icon' />&nbsp<i class='fas fa-users scoreboard-icon' />
-Highest score after time is up, wins
+// Draw the word displayed at the top left
+// Doubletap to zoom in/out
+// You must be zoomed in to draw
+// +1 point for guessing a drawing correctly
+// +1 point if your drawing is guessed
+// Draw or guess at any time
+// View scoreboard by tapping&nbsp<i class='fas fa-caret-down scoreboard-icon' />&nbsp<i class='fas fa-users scoreboard-icon' />
+// Highest score after time is up, wins
