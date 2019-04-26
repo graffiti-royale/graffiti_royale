51ea04c70cac51c8e558eb56195a0efdd10a8fc8
Rob Taylor <rob.taylor543@gmail.com>
2019-04-26 15:41:04 -0400

    Removed registration redux and sendgrid from pipfile

--- graffiti_royale/settings.py
+++ graffiti_royale/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'registration',
     'channels',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -92,9 +91,6 @@ DATABASES = {
 
 CONN_MAX_AGE = 0
 
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -137,10 +133,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-
-ACCOUNT_ACTIVATION_DAYS = 7
-LOGIN_REDIRECT_URL = '/'
 
 ASGI_APPLICATION = "graffiti_royale.routing.application"
 CHANNEL_LAYERS = {
