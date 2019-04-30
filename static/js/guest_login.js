01caa0ddc86dafa46df3e2d974c6b2b425e009c2
MichaelTaylor96 <michael96taylor@gmail.com>
2019-04-30 11:54:31 -0400

    Set username char limit to 15

--- static/js/guest_login.js
+++ static/js/guest_login.js
@@ -30,21 +30,25 @@ function guestLoginJavaScript () {
 
   document.querySelector('button').addEventListener('click', function () {
     let name = input.value
-    fetch('/checkguestname/', {
-      method: 'POST',
-      body: JSON.stringify({ 'username': name }),
-      headers: {
-        'X-CSRFToken': Cookies.get('csrftoken'),
-        'Content-Type': 'application/json'
-      } })
-      .then(response => response.json())
-      .then(function (data) {
-        if (data['url']) {
-          window.location.href = `https://${window.location.host}/${data['url']}/`
-        } else {
-          document.querySelector('h2').innerText = data['message']
-        }
-      })
+    if (name.length > 15) {
+      document.querySelector('h2').innerText = 'Character limit is 15'
+    } else {
+      fetch('/checkguestname/', {
+        method: 'POST',
+        body: JSON.stringify({ 'username': name }),
+        headers: {
+          'X-CSRFToken': Cookies.get('csrftoken'),
+          'Content-Type': 'application/json'
+        } })
+        .then(response => response.json())
+        .then(function (data) {
+          if (data['url']) {
+            window.location.href = `https://${window.location.host}/${data['url']}/`
+          } else {
+            document.querySelector('h2').innerText = data['message']
+          }
+        })
+    }
   })
 }
 
